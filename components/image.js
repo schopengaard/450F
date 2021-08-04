400642fdf8c19cdecff074d7669803e69c35117f
Bobby <806612681@qq.com>
2021-08-03 17:30:25 -0700

    image location

---
+++ components/image.js
@@ -0,0 +1,12 @@
+export default{
+  '1':require('../assets/img/books/1.png'),
+  '2':require('../assets/img/books/2.png'),
+  '3':require('../assets/img/books/3.png'),
+  '4':require('../assets/img/books/4.png'),
+  '5':require('../assets/img/books/5.png'),
+  '6':require('../assets/img/books/6.png'),
+  '7':require('../assets/img/books/7.png'),
+  '8':require('../assets/img/books/8.png'),
+  '9':require('../assets/img/books/9.png'),
+  '10':require('../assets/img/books/10.png'),
+};

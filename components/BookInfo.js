f57711190f65bddb16aaf0b96c65ce20b32b4b2b
schopengaard <j.schopengaard@gmail.com>
2021-08-04 04:29:14 -0700

    homepage content update

---
+++ components/BookInfo.js
@@ -0,0 +1,66 @@
+const BookInfo = {
+  data: [
+    {
+      image: require('../assets/img/books/1.png'),
+      title: 'The Song of Achilles',
+      author: 'Madeline Miller',
+      genre: 'Novel, Romance novel, Historical Fiction, War story',
+    },
+    {
+      image: require('../assets/img/books/2.png'),
+      title: 'The Nightingale',
+      author: 'Kristin Hannah',
+      genre: 'Historical fiction',
+    },
+    {
+      image: require('../assets/img/books/3.png'),
+      title: 'Survive the Night',
+      author: 'Riley Sager',
+      genre: 'Thriller, Suspense, Psychological thriller, Horror fiction',
+    },
+    {
+      image: require('../assets/img/books/4.png'),
+      title: 'The Intelligent Investor',
+      author: 'Benjamin Graham',
+      genre: 'Non-fiction, Securities, Investment',
+    },
+    {
+      image: require('../assets/img/books/5.png'),
+      title: 'The 7 Habits of Highly Effective People',
+      author: 'Stephen R. Covey',
+      genre: 'Non-fiction, Self-help',
+    },
+    {
+      image: require('../assets/img/books/6.png'),
+      title: 'Big Magic: How to Live a Creative Life, and Let Go of Your Fear',
+      author: 'Elizabeth Gilbert',
+      genre: 'Non-fiction, Self-help',
+    },
+    {
+      image: require('../assets/img/books/7.png'),
+      title: 'The Body Keeps the Score',
+      author: 'Bessel Van Der Kolk, M.D.',
+      genre: 'Non-fiction, Self-help',
+    },
+    {
+      image: require('../assets/img/books/8.png'),
+      title: 'When the Body Says No',
+      author: 'Gabor Maté M.D.',
+      genre: 'Non-fiction, Self-help',
+    },
+    {
+      image: require('../assets/img/books/9.png'),
+      title: 'The Untethered Soul',
+      author: 'Michael A. Singer',
+      genre: 'Non-fiction, Self-help',
+    },
+    {
+      image: require('../assets/img/books/10.png'),
+      title: 'Renegades: Born in the USA',
+      author: 'Barack Obama, Bruce Springsteen',
+      genre: 'Non-fiction, Self-help',
+    },
+  ],
+}
+
+export default BookInfo
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -15,6 +15,7 @@ import Search from '../components/Search'
 import Constants from 'expo-constants'
 import styles from '../components/Style.js'
 import { Col, Row, Grid } from 'react-native-easy-grid'
+import BookInfo from '../components/BookInfo'
 
 const HomePage = () => {
   return (
@@ -35,8 +36,37 @@ const HomePage = () => {
       </View>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <Text style={{ padding: 20, fontFamily: 'RobotoMono', fontSize: 20 }}>
-          YOUR READING INSIGHTS:
+          YOUR FAVOURITES:
         </Text>
+        <View style={{ flexDirection: 'column', justifyContent: 'flex-start' }}>
+          {BookInfo.data.map((data, index) => {
+            return (
+              <View style={{ flexDirection: 'row' }}>
+                <Image
+                  source={data.image}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    resizeMode: 'contain',
+                    margin: 10,
+                  }}
+                />
+                <View
+                  style={{
+                    flexShrink: 1,
+                    marginTop: 10,
+                    padding: 10,
+										paddingLeft: 0
+                  }}
+                >
+                  <Text style={{ fontSize: 18 }}>{data.title}</Text>
+                  <Text>{'by ' + data.author}</Text>
+									<Text style={{fontStyle: 'italic', fontSize: 12, color: '#aaa'}}>{'' + data.genre}</Text>
+                </View>
+              </View>
+            )
+          })}
+        </View>
       </ScrollView>
     </View>
   )
